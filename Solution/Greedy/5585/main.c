76824eb166a215aa4d5cba10d15d8063caaf4144
hhcczz <101077489+hhcczz@users.noreply.github.com>
2024-06-23 17:33:22 +0900

    Create main.c

---
+++ Solution/Greedy/5585/main.c
@@ -0,0 +1,24 @@
+int main()
+{
+	int N = 0;
+	int cnt = 0;
+
+	scanf("%d", &N);
+	
+	N = 1000 - N;
+
+	while (N != 0)
+	{
+		if (N >= 500) N -= 500;
+		else if (N >= 100) N -= 100;
+		else if (N >= 50) N -= 50;
+		else if (N >= 10) N -= 10;
+		else if (N >= 5) N -= 5;
+		else if (N >= 1) N -= 1;
+		cnt++;
+	}
+
+	printf("%d", cnt);
+
+	return 0;
+}
