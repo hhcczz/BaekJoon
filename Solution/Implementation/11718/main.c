5a4b42da34dcf9e6ef09b8ed06a411b4b0604e14
hhcczz <101077489+hhcczz@users.noreply.github.com>
2024-08-11 21:50:00 +0900

    Create main.c

---
+++ Solution/Implementation/11718/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int main()
+{
+    char Arr_1;
+
+    while(scanf("%c", &Arr_1) != EOF)
+    {
+        printf("%c", Arr_1);
+    }
+
+    return 0;
+}
