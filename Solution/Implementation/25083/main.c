e44db32d1cbccf4e71fa6a681fe81cbdf983d40e
hhcczz <101077489+hhcczz@users.noreply.github.com>
2024-08-16 21:40:40 +0900

    Create main.c

---
+++ Solution/Implementation/25083/main.c
@@ -0,0 +1,11 @@
+int main()
+{
+    printf("         ,r'\"7\n");
+    printf("r`-_   ,'  ,/\n");
+    printf(" \\. \". L_r'\n");
+    printf("   `~\\/\n");
+    printf("      |\n");
+    printf("      |");
+
+    return 0;
+}
